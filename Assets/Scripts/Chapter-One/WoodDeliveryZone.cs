f6d6849adb6485f66c149a42edff26c987c7444a
AngeloCarla <carladalilaa@gmail.com>
2025-11-12 17:05:09 -0300

    Update

---
+++ Assets/Scripts/Chapter-One/WoodDeliveryZone.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WoodDeliveryZone : MonoBehaviour
+{
+    [SerializeField] private CollectWoodMission mission;
+    public void RepairBridge()
+    {
+        if (mission == null)
+        {
+            Debug.LogWarning("⚠️ No se asignó la misión al puente.");
+            return;
+        }
+
+        if (mission.HasEnoughWood())
+        {
+            Debug.Log("Usas la madera recolectada para reparar el puente...");
+            StartCoroutine(RepairBridgeRoutine());
+        }
+        else
+        {
+            Debug.Log("❌ Aún no tienes suficiente madera para reconstruir el puente.");
+        }
+    }
+
+    private IEnumerator RepairBridgeRoutine()
+    {
+        Debug.Log("Reparando...");
+        yield return new WaitForSeconds(3f);
+        mission.CompleteMission();
+        Destroy(gameObject);
+    }
+}
