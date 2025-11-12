32849aff357b0c0f7d1e68255a20b0e0d80f6001
AngeloCarla <carladalilaa@gmail.com>
2025-11-12 19:08:55 -0300

    Mision de recoleccion
    Ajustes y pruebas en la mision de recoleccion

---
+++ Assets/Scripts/Chapter-One/WoodDelivery.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WoodDelivery : MonoBehaviour
+{
+    [SerializeField] private CollectWoodMission mission;
+    public void RepairBridge()
+    {
+        if (mission == null)
+        {
+            Debug.LogWarning("No se asignó la misión al puente");
+            return;
+        }
+
+        mission.DeliverWood();
+
+        if (mission.HasEnoughWood())
+        {
+            Debug.Log("Ya tienes suficiente!");
+            StartCoroutine(RepairBridgeRoutine());
+        }
+        else
+        {
+            Debug.Log("Aún no tienes suficiente madera");
+        }
+    }
+
+    private IEnumerator RepairBridgeRoutine()
+    {
+        Debug.Log("Reparando...");
+        yield return new WaitForSeconds(3f);
+        mission.CompleteMission();
+        Debug.Log("Mision completa!");
+        Destroy(gameObject);
+    }
+}
