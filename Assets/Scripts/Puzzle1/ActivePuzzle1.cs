af51b61e0b54fa0680e99dd37d9b7fecc05b9c5b
AngeloCarla <carladalilaa@gmail.com>
2025-11-25 17:45:01 -0300

    Actualizacion Interaccion
    Se modifica el sistema de interaccion a una version mas corta y se eliminan algunos script

---
+++ Assets/Scripts/Puzzle1/ActivePuzzle1.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ActivePuzzle1 : MonoBehaviour
+{
+    [Header("Mision")]
+    [SerializeField] private AmbitionPuzzleManager mission;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("¿De verdad te basta con esto?");
+
+            mission.gameObject.SetActive(true); // Activa la mision
+            Destroy(gameObject); // Destruye para evitar repetirla
+        }
+    }
+}
--- Assets/Scripts/Puzzle1/Fragment.cs
+++ Assets/Scripts/Puzzle1/Fragment.cs
@@ -1,29 +1,29 @@
 using UnityEngine;
 
-public class PickUpObject : MonoBehaviour
+public class Fragment : MonoBehaviour, IInteractable
 {
     // Script base para cualquier objeto que el jugador pueda recoger o eliminar del escenario 
-    [Header("Configuracion")]
-    public string objectName = "Objeto";
+    [Header("Fragmento Extra")]
     [SerializeField] private bool isExtra = false;
 
     private AmbitionPuzzleManager puzzle1;
     void Start()
     {
         puzzle1 = FindAnyObjectByType<AmbitionPuzzleManager>();
-        objectName = gameObject.name;
     }
 
-    public void PickUp()
+    public void PickUpFragment()
     {
-        // Ejemplo: "Recoge" el objeto
-        Debug.Log($"Recogiste {objectName}");
-
         if (puzzle1 != null)
         {
             puzzle1.AddFragment(isExtra);
         }
 
         Destroy(gameObject);
+    }
+
+    public void Interact()
+    {
+        PickUpFragment();
     }
 }
